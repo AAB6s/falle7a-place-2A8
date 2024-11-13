9fa2ca24ca835a585041e1dadd941d49d6f30755
AAB6s <aab627092003@gmail.com>
2024-11-13 19:45:56 +0100

    Latest update

---
+++ View/FRONT-OFFICE/script.js
@@ -0,0 +1,28 @@
+const wrapper = document.querySelector('.wrapper');
+const registerLink = document.querySelector('.register-link');
+const loginLink = document.querySelector('.login-link'); 
+registerLink.onclick = () => {
+    wrapper.classList.add('active');
+}
+
+loginLink.onclick = () => {
+    wrapper.classList.remove('active');
+}
+document.addEventListener("DOMContentLoaded", function() {
+    const loginForm = document.querySelector(".form-box.login form");
+  
+    loginForm.addEventListener("submit", function(event) {
+      event.preventDefault(); 
+  
+      const username = loginForm.querySelector('input[type="text"]').value;
+      const password = loginForm.querySelector('input[type="password"]').value;
+  
+      
+      if (username === "user" && password === "pass") {
+        localStorage.setItem("loggedIn", "true");
+        window.location.href = "index.html";
+      } else {
+        alert("Invalid username or password");
+      }
+    });
+  });
